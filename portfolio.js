435bff0c6bff520a04d5f4c15a11c6f9c6f8e7ea
Veeresh VK <104710293+7483140170@users.noreply.github.com>
2023-07-20 16:35:55 +0530

    first commit

--- portfolio.js
+++ portfolio.js
@@ -7,3 +7,15 @@ function showProjects(skill) {
     var selectedSkillContainer = document.getElementById(skill + "-projects");
     selectedSkillContainer.style.display = "block";
   }
+// Get the custom cursor element
+const customCursor = document.querySelector('.custom-cursor');
+
+// Function to update the custom cursor position
+function updateCursorPosition(e) {
+  // Update the cursor position to match the mouse position
+  customCursor.style.left = `${e.clientX}px`;
+  customCursor.style.top = `${e.clientY}px`;
+}
+
+// Event listener to track mouse movement and update cursor position
+document.addEventListener('mousemove', updateCursorPosition);
